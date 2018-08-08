592182e7d59e44f07d161bacd7475edceccf3114
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-08-08 09:20:10 -0500

    wip

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql://root@localhost/circle_test")
+# engine = create_engine("postgresql://root@localhost/circle_test")
+
+df = pd.DataFrame({"A": [1, 2]})
+url = "postgresql+psycopg2://postgres@localhost/pandas_nosetest"
+
+engine = create_engine(url)
+
+df.to_sql('test_a', engine)
