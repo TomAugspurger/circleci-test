92b89ff8d0cd0d9848d56fcae84aa0ee7dc12d11
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-08-08 09:26:58 -0500

    mysql

--- test.py
+++ test.py
@@ -9,3 +9,14 @@ url = "postgresql+psycopg2://postgres@localhost/pandas_nosetest"
 engine = create_engine(url)
 
 df.to_sql('test_a', engine)
+result = pd.read_sql_query('select * from test_a', engine)
+print("Postgres worked")
+
+
+url = "mysql+pymysql://root@localhost/pandas_nosetest"
+mysql = create_engine(url)
+
+df.to_sql('test_a', mysql)
+result = pd.read_sql_query('select * from test_a', mysql)
+print(result)
+print("mysql worked")
